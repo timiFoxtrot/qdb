f60ece76c8df68b851049e66c081c6830fc3c74d
Timi Ajibade <timiajibade24@gmail.com>
2024-12-11 15:16:36 +0100

    added prompt

--- Task-1/temperatureConverter.js
+++ Task-1/temperatureConverter.js
@@ -1,3 +1,8 @@
+const prompt = require('prompt-sync')();
+
+const temp = prompt('Enter temperature value: ')
+const unit = prompt('Choose conversion: "C" for Celsius to Fahrenheit, "F" for Fahrenheit to Celsius: ')
+
 const tempConverter = (temp, unit) => {
     if (!temp || !unit) return 'Provide missing parameter'
     let tempF
@@ -5,8 +10,8 @@ const tempConverter = (temp, unit) => {
         tempF = ((temp * (9/5)) + 32 + 'ºF')
     } else if (unit.toLowerCase() === "f") {
         tempF = ((temp - 32) * (5/9) + 'ºC')
-    }
-    return tempF
+    } else return 'invalid unit'
+    return `Converted temperature: ${tempF}`
 }
 
-console.log(tempConverter(30))
+console.log(tempConverter(temp, unit))
--- Task-1/temperatureConverter.ts
+++ Task-1/temperatureConverter.ts
@@ -1,11 +1,19 @@
-const tempConverter = (temp: number, unit: string) => {
-    let tempF: string
-    if (unit.toLowerCase() === "c") {
-        tempF = ((temp * (9/5)) + 32 + 'ºF')
-    } else if (unit.toLowerCase() === "f") {
-        tempF = ((temp - 32) * (5/9) + 'ºC')
-    } else return 'invalid unit'
-    return tempF
-}
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 
-console.log(tempConverter(30, 'c'))
+const temp: string = prompt("Enter temperature value: ");
+const unit: string = prompt(
+  'Choose conversion: "C" for Celsius to Fahrenheit, "F" for Fahrenheit to Celsius: '
+);
+
+const tempConverter = (temp: number, unit: string) => {
+  let tempF: string;
+  if (unit.toLowerCase() === "c") {
+    tempF = temp * (9 / 5) + 32 + "ºF";
+  } else if (unit.toLowerCase() === "f") {
+    tempF = (temp - 32) * (5 / 9) + "ºC";
+  } else return "invalid unit";
+  return `Converted temperature: ${tempF}`;
+};
+
+console.log(tempConverter(Number(temp), unit));
